0b19ada8be13a972be31b5ae82b04371aa8b55e3
Different Name <comein22>
2012-09-12 18:05:08 +1000

    w3c checking services

--- core/w3c.py
+++ core/w3c.py
@@ -1,22 +1,48 @@
 import urllib
 import urllib2
 
-URL = "http://validator.w3.org/check?uri=%s"
-SITE_URL = "http://www.netregistry.com.au"
 
-# pattern for HEAD request taken from
-# http://stackoverflow.com/questions/4421170/python-head-request-with-urllib2
+def w3c_markup_check(domain):
+    URL = "http://validator.w3.org/check?uri=%s"
+    SITE_URL = "http://{0}".format(domain)
+    # pattern for HEAD request taken from
+    # http://stackoverflow.com/questions/4421170/python-head-request-with-urllib2
+    request = urllib2.Request(URL % urllib.quote(SITE_URL))
+    request.get_method = lambda : 'HEAD'
+    response = urllib2.urlopen(request)
 
-request = urllib2.Request(URL % urllib.quote(SITE_URL))
-request.get_method = lambda : 'HEAD'
-response = urllib2.urlopen(request)
+    result = {}
 
-valid = response.info().getheader('X-W3C-Validator-Status')
-if valid == "Valid":
-    valid = True
-else:
-    valid = False
-errors = int(response.info().getheader('X-W3C-Validator-Errors'))
-warnings = int(response.info().getheader('X-W3C-Validator-Warnings'))
+    valid = response.info().getheader('X-W3C-Validator-Status')
+    if valid == "Valid":
+        result['valid'] =  True
+    else:
+        result['valid'] =  False
 
-print "Valid markup: %s (Errors: %i, Warnings: %i) " % (valid, errors, warnings)
+    result['X-W3C-Validator-Errors'] =   int(response.info().getheader('X-W3C-Validator-Errors'))
+    result['X-W3C-Validator-Warnings'] =   int(response.info().getheader('X-W3C-Validator-Warnings'))
+
+    return result
+
+
+#todo merge into one method
+def w3c_css_check(domain):
+    URL =  "http://jigsaw.w3.org/css-validator/validator?uri=%s"
+    SITE_URL = "http://{0}".format(domain)
+    # pattern for HEAD request taken from
+    # http://stackoverflow.com/questions/4421170/python-head-request-with-urllib2
+    request = urllib2.Request(URL % urllib.quote(SITE_URL))
+    request.get_method = lambda : 'HEAD'
+    response = urllib2.urlopen(request)
+
+    result = {}
+    valid = response.info().getheader('X-W3C-Validator-Status')
+    if valid == "Valid":
+        result['valid'] =  True
+    else:
+        result['valid'] =  False
+
+    result['X-W3C-Validator-Errors'] =   int(response.info().getheader('X-W3C-Validator-Errors'))
+
+
+    return result
