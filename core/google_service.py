fda6427d0cbc25693a899b07724462259a4b501e
facingnorth <facingnorth@facingnorth.com>
2012-10-04 23:29:51 +1000

    fixed google 503

--- core/google_service.py
+++ core/google_service.py
@@ -5,56 +5,64 @@ logger = logging.getLogger(__name__)
 
 
 def google_backlinks(domain):
-    logger.info("begin google_backlinks for domain %s" % domain)
-    import urllib2
-    from BeautifulSoup import BeautifulSoup
-    #from http://stackoverflow.com/questions/802134/changing-user-agent-on-urllib2-urlopen
-    url = "https://www.google.com/search?q=%s".format(domain,domain)
-    keyword = '"{0}" -site:{1}'.format(domain,domain)
-    url = (url % urllib.quote_plus(keyword))
-    logger.info(url)
-    headers = { 'User-Agent' : 'Mozilla/5.0' }
-    req = urllib2.Request(url, None, headers)
-    html = urllib2.urlopen(req).read()
+    try:
+        logger.info("begin google_backlinks for domain %s" % domain)
+        import urllib2
+        from BeautifulSoup import BeautifulSoup
+        #from http://stackoverflow.com/questions/802134/changing-user-agent-on-urllib2-urlopen
+        url = "https://www.google.com/search?q=%s".format(domain,domain)
+        keyword = '"{0}" -site:{1}'.format(domain,domain)
+        url = (url % urllib.quote_plus(keyword))
+        logger.info(url)
+        headers = { 'User-Agent' : 'Mozilla/5.0' }
+        req = urllib2.Request(url, None, headers)
+        html = urllib2.urlopen(req).read()
 
-    soup = BeautifulSoup(html)
-    soup.prettify()
-    result =soup.find("div",{"id": "resultStats"}).string
-    logger.info("Results %s" %  result)
-    if result:
-        if result=='1 result':
-            return 1
-        result = result.lower().replace("about ","")
-        result = result.replace(" results","")
-        result = result.replace(',','')
-    else:
-        result= 0
+        soup = BeautifulSoup(html)
+        soup.prettify()
+        result =soup.find("div",{"id": "resultStats"}).string
+        logger.info("Results %s" %  result)
+        if result:
+            if result=='1 result':
+                return 1
+            result = result.lower().replace("about ","")
+            result = result.replace(" results","")
+            result = result.replace(',','')
+        else:
+            result= 0
 
-    logger.info("google_backlinks domain %s results %s" % (domain, result))
-    return result
+        logger.info("google_backlinks domain %s results %s" % (domain, result))
+        return result
+    except Exception,e:
+        logger.error(e)
+        return -1
 
 def google_indexed(domain):
-    logger.info("begin google_indexed for domain %s" % domain)
-    import urllib2
-    from BeautifulSoup import BeautifulSoup
-    #from http://stackoverflow.com/questions/802134/changing-user-agent-on-urllib2-urlopen
-    url = "https://www.google.com/search?q=site:{1}".format(domain,domain)
-    logger.info(url)
-    headers = { 'User-Agent' : 'Mozilla/5.0'}
-    req = urllib2.Request(url, None, headers)
-    html = urllib2.urlopen(req).read()
-    soup = BeautifulSoup(html)
-    soup.prettify()
-    result =soup.find("div",{"id": "resultStats"}).string
-    logger.info("Results %s" %  result)
-    if result:
-        if result=='1 result':
-            return 1
-        result = result.lower().replace("about ","")
-        result = result.replace(" results","")
-        result = result.replace(',','')
-    else:
-        result= 0
+    try:
+        logger.info("begin google_indexed for domain %s" % domain)
+        import urllib2
+        from BeautifulSoup import BeautifulSoup
+        #from http://stackoverflow.com/questions/802134/changing-user-agent-on-urllib2-urlopen
+        url = "https://www.google.com/search?q=site:{1}".format(domain,domain)
+        logger.info(url)
+        headers = { 'User-Agent' : 'Mozilla/5.0'}
+        req = urllib2.Request(url, None, headers)
+        html = urllib2.urlopen(req).read()
+        soup = BeautifulSoup(html)
+        soup.prettify()
+        result =soup.find("div",{"id": "resultStats"}).string
+        logger.info("Results %s" %  result)
+        if result:
+            if result=='1 result':
+                return 1
+            result = result.lower().replace("about ","")
+            result = result.replace(" results","")
+            result = result.replace(',','')
+        else:
+            result= 0
 
-    logger.info("google_indexed domain %s results %s" % (domain, result))
-    return result
+        logger.info("google_indexed domain %s results %s" % (domain, result))
+        return result
+    except Exception,e:
+        logger.error(e)
+        return -1
